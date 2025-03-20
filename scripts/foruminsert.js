22e2c34f38218c2680843a44efa8872d2bc92b2a
Joseeul <jose_andreas@icloud.com>
2025-03-21 00:03:27 +0700

    aduhai cape

--- scripts/foruminsert.js
+++ scripts/foruminsert.js
@@ -4,23 +4,6 @@ document.addEventListener("DOMContentLoaded", () => {
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5aXZrYXRsdml1d2l0d2prbGtxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIzMTYxMDMsImV4cCI6MjA1Nzg5MjEwM30.nLQ_vGBgSGWqDMz1LF1vSWAeydpxItyT_ZDvwwc9IQQ";
   const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
-  // async function showForum() {
-  //   const { data, error } = await supabase
-  //     .from("forum")
-  //     .select("title, content, username");
-
-  //   if (error) {
-  //     alert("Failed to load posts.");
-  //     console.error("Error fetching posts:", error.message);
-  //   } else {
-  //     document.getElementById("forumContent").textContent = JSON.stringify(
-  //       data,
-  //       null,
-  //       2
-  //     );
-  //   }
-  // }
-
   async function showForum() {
     const { data, error } = await supabase
       .from("forum")
@@ -38,10 +21,22 @@ document.addEventListener("DOMContentLoaded", () => {
     data.forEach((post) => {
       const postDiv = document.createElement("div");
       postDiv.innerHTML = `
-            <h3>${post.title}</h3>
-            <p><strong>By:</strong> ${post.username}</p>
-            <p>${post.content}</p>
-            <hr>
+      <div class="card-parent w-100 mb-5">
+        <div class="card mb-3">
+          <div class="card-body">
+            <h5 class="card-title">${post.title}</h5>
+            <p class="card-text">${post.content}
+            </p>
+          </div>
+        </div>
+        <div class="button-card d-flex justify-content-between align-items-center mt-2">
+          <h6 class="card-subtitle text-body-secondary">${post.username}'s Forum</h6>
+          <div class="info-parent">
+            <button class="me-2 card-subtitle text-body-secondary">Calculus</button>
+            <button class="card-subtitle text-body-secondary">Computer Science</button>
+          </div>
+        </div>
+      </div>
         `;
       forumContent.appendChild(postDiv);
     });
